67134901e127ae82eec72112bbb216da8b61e386
AndresWnj <andres.wnj@gmail.com>
2022-11-22 19:00:50 -0500

    primer commit

---
+++ httpx/assets/js/logica/routes/routes.js
@@ -0,0 +1,10 @@
+const URL_API_BASE = "https://wallpay.app/index.html";
+const URL_API_SUPER_BASE = "http://52.175.225.67:8094/api"
+
+
+
+
+
+const URL_API_LOGIN_USUARIOS = URL_API_SUPER_BASE + "/Registro";
+const URL_API_LOGIN = URL_API_SUPER_BASE + "/IniciarSesion";
+const URL_API_REGISTRO_USUARIOS = URL_API_SUPER_BASE + "/Registro"; 
