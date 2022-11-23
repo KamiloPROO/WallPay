821266ed2c9ac4337e3c4a67df517b0d8315ca60
AndresWnj <andres.wnj@gmail.com>
2022-11-23 15:35:16 -0500

    crear cuenta tron listo

--- assets/js/logica/routes/routes.js
+++ assets/js/logica/routes/routes.js
@@ -1,6 +1,6 @@
 const URL_API_BASE = "https://wallpay.app/index.html";
-const URL_API_SUPER_BASE = "http://52.175.225.67:8094/api";
-const URL_API_BASE_CREAR_TRON = "http://52.175.225.67:8094/Api";
+const URL_API_SUPER_BASE = "https://appwollpay.app/api";
+const URL_API_BASE_CREAR_TRON = "https://appwollpay.app/Api";
 
 
 
