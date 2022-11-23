71354a9479c1dd3bed8f4b8e0c3daecb90ab35bb
AndresWnj <andres.wnj@gmail.com>
2022-11-23 11:21:45 -0500

    crear cuenta tron listo

--- assets/js/logica/routes/routes.js
+++ assets/js/logica/routes/routes.js
@@ -1,5 +1,6 @@
 const URL_API_BASE = "https://wallpay.app/index.html";
-const URL_API_SUPER_BASE = "http://52.175.225.67:8094/api"
+const URL_API_SUPER_BASE = "http://52.175.225.67:8094/api";
+const URL_API_BASE_CREAR_TRON = "http://52.175.225.67:8094/Api";
 
 
 
@@ -8,3 +9,4 @@ const URL_API_SUPER_BASE = "http://52.175.225.67:8094/api"
 const URL_API_LOGIN_USUARIOS = URL_API_SUPER_BASE + "/Registro";
 const URL_API_LOGIN = URL_API_SUPER_BASE + "/IniciarSesion";
 const URL_API_REGISTRO_USUARIOS = URL_API_SUPER_BASE + "/Registro"; 
+const URL_API_CREAR_CUENTA_TRON = URL_API_BASE_CREAR_TRON + "/AddCuentaTron";
--- assets/js/logica/utils/HttpsLogin.js
+++ assets/js/logica/utils/HttpsLogin.js
@@ -4,30 +4,35 @@ function CallEndPointPost(url, jsonArgs) {
         url: url,
         data: jsonArgs,
         success: function (data) {
-            
+
             console.log(data);
 
             // var formData = JSON.stringify(data, null ,2);
             var formData = JSON.parse(data);
 
-            // console.log(formData.Response);
-    
+            console.log(formData);
+
+            const idcfLogin = formData.IdCf;
+            const tokenLogin = formData.Token;
+
+            // console.log( idcfLogin + " | " + tokenLogin);
 
             // console.log(formData.Respose);
-            if (formData.Response === 707){
+            if (formData.Response === 707) {
 
+
+                localStorage.setItem("idCfLogin", idcfLogin);
+                localStorage.setItem("tokenLogin", tokenLogin);
                 window.location.href = URL_API_BASE;
                 location.replace("Dash.html");
-                location.href ="Dash.html";
+                location.href = "Dash.html";
 
-                // console.log(formData);
-
-            }else{
+            } else {
                 Swal.fire({
                     icon: 'error',
                     title: 'Oops...',
                     text: 'Usuario o Contraseña Incorrectas',
-                    
+
                 })
             }
 
@@ -35,7 +40,7 @@ function CallEndPointPost(url, jsonArgs) {
 
         },
         error: function () {
-            alert("Ha ocurrido un error, por favor vuelva a intentarlo")            
+            alert("Ha ocurrido un error, por favor vuelva a intentarlo")
         }
     });
 }
